8cfb9f9e033bfcc2622713b845fa78ee5bb1c9ea
chernykh.a.n@edu.mirea.ru <N3s_d45o823*b1a>
2021-11-16 07:12:26 +0300

    all done

---
+++ src/com/company/Square.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Square extends Rectangle{
+    public Square() {}
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public double getSide() {
+        return this.width;
+    }
+
+    public void setSide(double side) {
+        this.width = side;
+        this.length = side;
+    }
+
+    public void setWidth(double side) {
+        super.setWidth(side);
+    }
+
+    public void setLength(double side) {
+        super.setLength(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + width +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
